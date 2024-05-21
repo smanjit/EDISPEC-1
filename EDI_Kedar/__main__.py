86f6b7f26e063a2594ce2554972ed797a87530de
GayathriG1985 <61275765+GayathriG1985@users.noreply.github.com>
2024-05-21 18:23:39 +0530

    Update __main__.py

--- EDI_Kedar/__main__.py
+++ EDI_Kedar/__main__.py
@@ -24,7 +24,37 @@ cos = ibm_boto3.resource("s3",
     endpoint_url=COS_ENDPOINT
 )
 
-      
+
+
+def multi_part_upload(bucket_name, item_name, file_data):
+    try:
+        print("Starting file transfer for {0} to bucket: {1}\n".format(item_name, bucket_name))
+        # set 5 MB chunks
+        part_size = 1024 * 1024 * 5
+
+        # set threadhold to 15 MB
+        file_threshold = 1024 * 1024 * 15
+
+        # set the transfer threshold and chunk size
+        transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+            multipart_threshold=file_threshold,
+            multipart_chunksize=part_size
+        )
+
+        # the upload_fileobj method will automatically execute a multi-part upload
+        # in 5 MB chunks for all files over 15 MB
+        # with open(file_path, "rb") as file_data:
+        cos.Object(bucket_name, item_name).upload_fileobj(
+            Fileobj=file_data,
+            Config=transfer_config
+        )
+
+        print("Transfer for {0} Complete!\n".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to complete multi-part upload: {0}".format(e))
+        
 
 def main(args):
     print("Invoked main");
