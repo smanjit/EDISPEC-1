6f88678d794d57b628944c13959d4d42bef1f985
GayathriG1985 <61275765+GayathriG1985@users.noreply.github.com>
2024-05-22 17:11:26 +0530

    Update __main__.py

--- EDI_Kedar/__main__.py
+++ EDI_Kedar/__main__.py
@@ -1,8 +1,13 @@
-def main(args):
-    return {
+def main(params):
+     return {
+          # specify headers for the HTTP response
+          # we only set the Content-Type in this case, to 
+          # ensure the text is properly displayed in the browser
           "headers": {
               "Content-Type": "text/plain;charset=utf-8",
           },
-          "body": "success"
-          }
-
+          
+          ## use the text generator to create a response sentence
+          #  with 10 words
+          "body": "God bless you",
+      }
