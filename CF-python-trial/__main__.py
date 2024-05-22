8197ce32f426a0581ad0f02e26182efa8c660282
GayathriG1985 <61275765+GayathriG1985@users.noreply.github.com>
2024-05-22 17:27:32 +0530

    Update __main__.py

--- CF-python-trial/__main__.py
+++ CF-python-trial/__main__.py
@@ -1,6 +1,22 @@
 from neo4j import GraphDatabase
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
 
 def main(params):
+
+     # Constants for IBM COS values
+     COS_ENDPOINT = "https://s3.us-south.cloud-object-storage.appdomain.cloud" # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
+     COS_API_KEY_ID = "9ITYiSk-BpydNpRFnwTczyliKE5VFqaTZEPtkhI_5MeH" # eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
+     COS_INSTANCE_CRN = "crn:v1:bluemix:public:iam-identity::a/010dcec46cce40a9a8555682c8c82e84::serviceid:ServiceId-f61fe320-d830-4f83-bb07-e31daa91878d" # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003xxxxxxxxxx1c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
+
+     # Create resource
+     cos = ibm_boto3.resource("s3",
+     ibm_api_key_id=COS_API_KEY_ID,
+     ibm_service_instance_id=COS_INSTANCE_CRN,
+     config=Config(signature_version="oauth"),
+     endpoint_url=COS_ENDPOINT
+     )
+
      neo4j_uri = 'neo4j+s://a2903c3d.databases.neo4j.io'
      neo4j_user = 'neo4j'
      neo4j_password = 'x7lO8GKrglcmm4MYuHcBp_PJx23STanbAUKfnuj_FIg'
